6a502b22ce161078c5af0cdea66eac02faa04ed1
Dan Leach <web@danleach.uk>
2021-11-12 13:04:00 +0000

    Create footer.php

---
+++ php-1.2.0/footer.php
@@ -0,0 +1,7 @@
+<div class="well"><!--Footer-->
+	<strong>Paediatric DKA Calculator <a href="https://github.com/dan-leach/dka-calculator/blob/master/changelog.md" target="_blank">(Version 1.2.0 - UNDER DEVELOPMENT - NOT FOR CLINICAL USE)</a></strong>
+	<br>Created by <a href="https://danleach.uk" target="_blank">Dan Leach</a> and John Barton with the BSPED DKA Special Interest Group
+	<br>Correspondence: <a href="mailto:admin@dka-calculator.co.uk">admin@dka-calculator.co.uk</a>
+	<br><br><b> Important: Decisions about patient care remains the treating clinician's responsibility. By using this website you confirm that you accept in full the terms of the disclaimer. If you do not agree to the terms, you must not use this site.</b>
+	<br><br><a href="#" tabindex="-1" data-toggle="tooltip" title="This replaces CE marking for medical devices following Brexit."><img src="/images/UKCA-mark.png" alt="CE Mark" width="40"></a> The BSPED Paediatric DKA Calculator is registered as a medical device in the UK.
+</div>
