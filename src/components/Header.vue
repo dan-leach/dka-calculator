fce37724dbb3124f1c437ae983a834e2b717f240
dan-leach <web@danleach.uk>
2024-04-16 05:21:16 +0100

    Header improved layout for mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,12 +4,12 @@ import { RouterLink } from "vue-router";
 
 <template>
   <nav id="header" class="navbar bg-grey">
-    <div class="container-fluid m-1">
+    <div class="container-fluid">
       <RouterLink
         to="/"
-        class="navbar-brand d-flex flex-row flex-wrap align-items-center"
-        ><img alt="BSPED logo" class="logo me-4" src="@/assets/bsped.png" />
-        <h1 class="site-title">Paediatric DKA Calculator</h1></RouterLink
+        class="navbar-brand d-flex flex-row flex-wrap align-items-center justify-content-center"
+        ><img alt="BSPED logo" class="logo" src="@/assets/bsped.png" />
+        <h1 class="display-5 mx-4 my-0">Paediatric DKA Calculator</h1></RouterLink
       >
     </div>
   </nav>
@@ -17,14 +17,17 @@ import { RouterLink } from "vue-router";
 
 <style scoped>
 .logo {
-  width: 20vw;
+  width: 10vw;
 }
 .site-title {
-  font-size: 4vw;
+  font-size: 3vw;
   margin: 0;
   margin-left: 10px;
 }
 .navbar {
   padding: 0;
 }
+@media only screen and (max-width: 600px) {.logo {
+  width: 40vw;
+}}
 </style>
